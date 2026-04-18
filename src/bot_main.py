86db73f170c71a81ac7e9621303372cb6af7b201
maxGrigorenko <50823330+maxGrigorenko@users.noreply.github.com>
2026-04-18 16:57:26 +0500

    added refactoring plan to git; added TODO plan for bugs in bot_main

--- src/bot_main.py
+++ src/bot_main.py
@@ -145,7 +145,7 @@ class SmallBly(BotAI):
     def read_and_choose_strategy(self):
         opponent_id = self.opponent_id
         if opponent_id is None:
-            self.strategy = StrategyID.RAVAGER_RUSH
+            self.strategy = StrategyID.ZERGLING_DRONE_RUSH
             return
 
         with open("data/statistics.txt") as f:
@@ -224,12 +224,20 @@ class SmallBly(BotAI):
         self.action_registry.clear()
 
 
+'''
+TODO:
+1) bug, after defending against scout probe drones stop and do nothing (see games against nida)
+2) bug, when drone can't start building roach warren in roach_rush (also see after defending against worker rush) (see 48 minutes game against JimmyBot)
+3) bug, when queens do not attack (see Tie against BenBotBC)
+'''
+
+
 def main():
-    run_game(sc2.maps.get("TorchesAIE_v4"), [  # 2000AtmospheresAIE ; CatalystLE ; AbyssalReefLE
-        # Human(Race.Protoss),                         # JagannathaAIE ; BlackburnAIE ; OxideAIE ; PersephoneAIE_v4
-        # Bot(Race.Zerg, SmallBly()),               # TorchesAIE_v4
+    run_game(sc2.maps.get("MagannathaAIE_v2"), [  # 2000AtmospheresAIE ; CatalystLE ; AbyssalReefLE
+        Human(Race.Zerg),                         # JagannathaAIE ; BlackburnAIE ; OxideAIE ; PersephoneAIE_v4
+        # Bot(Race.Zerg, SmallBly()),               # TorchesAIE_v4; PylonAIE_v4; MagannathaAIE_v2
         Bot(Race.Zerg, SmallBly()),
-        Computer(Race.Protoss, Difficulty.VeryHard),
+        # Computer(Race.Terran, Difficulty.CheatInsane),
     ], realtime=False,
              disable_fog=False,
              random_seed=0,
